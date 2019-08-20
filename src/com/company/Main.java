c5090f6031fafcccfcfe918d9ccadf31def2271a
TylerCEdge <tylercedge@gmail.com>
2019-08-20 19:50:17 -0400

    adding coding back to this file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue - Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+        // Overflow
+        System.out.println("Busted MAX value: " + (myMaxIntValue + 1));
+
+        // Underflow
+        System.out.println("Busted MIN value: " + (myMinIntValue -1));
     }
 }
